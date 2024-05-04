e2d24b1b610c2f10f83bcfbe65c1611f4672ff8b
Dayne Wright <daynewr@gmail.com>
2024-05-04 10:12:04 -0500

    move to live stripe

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,21 +6,10 @@
  */
 
 import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
       <div
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,14 @@ import StripeButton from "../components/stripeButton"
 import * as styles from "../components/index.module.css"
 
 const IndexPage = () => {
+  const [isDesktop, setDesktop] = React.useState(false)
+
+  React.useEffect(() => {
+    if (typeof window !== "undefined") {
+      setDesktop(window.innerWidth > 768)
+    }
+  }, [])
+
   return (
     <Layout>
       <Script async src="https://js.stripe.com/v3/buy-button.js"></Script>
@@ -41,29 +49,30 @@ const IndexPage = () => {
       </div>
       <div className={styles.container}>
         <div className={styles.sectionContainer}>
-          <h1>Options for Student Band Program 🎺</h1>
+          <h1>{isDesktop && "Options for"} Student Band Program 🎺</h1>
           <p>
-            This is a monthly payment until the total of <b>$780</b> is reached.
+            This is a monthly payment until the total of <b>$775</b> is reached.
             If you have multiple children in band, you will need to set up a
             payment for each child separately.
           </p>
           <div className={styles.sectionButtons}>
             <StripeButton
-              buttonId="buy_btn_1PC94cHaK9Zw5GBQGMbehrB6"
-              headerText="Pay in full!"
+              buttonId="buy_btn_1PCk3oHaK9Zw5GBQUcMR7ioQ"
+              headerText="Pay in full"
             />
             <StripeButton
-              buttonId="buy_btn_1PC952HaK9Zw5GBQ1QuFA7eA"
-              headerText="$260 monthly for 3 months."
+              buttonId="buy_btn_1PCk9SHaK9Zw5GBQbt3BCJGz"
+              headerText="$260 monthly for 3 months"
             />
             <StripeButton
-              buttonId="buy_btn_1PC95XHaK9Zw5GBQ6RAzS4we"
-              headerText="$130 monthly for 6 months."
+              buttonId="buy_btn_1PCkAcHaK9Zw5GBQoOLagEGV"
+              headerText="$130 monthly for 6 months"
             />
           </div>
         </div>
+        {!isDesktop && <div className={styles.line} />}
         <div className={styles.sectionContainer}>
-          <h1>Options for Student Meal Plan 🍽️</h1>
+          <h1>{isDesktop && "Options for"} Student Meal Plan 🍽️</h1>
           <h3 className={styles.dueDate}>
             <b>*DUE by Friday, June 28th*</b>
           </h3>
@@ -74,12 +83,12 @@ const IndexPage = () => {
           </p>
           <div className={styles.sectionButtons}>
             <StripeButton
-              buttonId="buy_btn_1PC9AVHaK9Zw5GBQgCQDJJ6c"
+              buttonId="buy_btn_1PCkBpHaK9Zw5GBQWdbXW2jw"
               headerText="Pay in full"
             />
             <StripeButton
-              buttonId="buy_btn_1PC9BNHaK9Zw5GBQCwMqcij5"
-              headerText="$130 monthly for 2 months."
+              buttonId="buy_btn_1PCkCjHaK9Zw5GBQeBOOLDGZ"
+              headerText="$130 monthly for 2 months"
             />
           </div>
         </div>
